ed6b6b4ec44f68b401f4b4f8e8ac1cb66529af1e
ultramathi <ultramathi@gmail.com>
2022-09-25 08:57:58 +0530

    1 file modified

--- index.js
+++ index.js
@@ -6,14 +6,19 @@ let b = 'MATHIVANAN';
 let c = [10, 20, 30];
 // c 10+20+30 = 60
 
-//Method 1
+/*
+1. Addition of Values in Array
+Input - [10, 20, 30]
+Output - 60
+*/
+//Method 1 - using repetative line of code
 
 let d = c[0];
 let e = c[1];
 let f = c[2];
 let g = console.log(d + e + f);
 
-//Method 2
+//Method 2 - using For loop
 let h = 0;
 for (i = 0; i <= c.length - 1; i++) {
   // i=0 ; 0 <= 3 - 1 = 2 ; h 10 ; i++
@@ -22,12 +27,20 @@ for (i = 0; i <= c.length - 1; i++) {
 }
 console.log(h);
 
-//Method 3
-let j = console.log(
-  c.reduce((acc, val) => acc + val)
-);
-//reduce array value to single array value
+//Method 3 - using Reduce Function
+let j = console.log(c.reduce((acc, val) => acc + val));
 
-
-
-
+/*
+2.Performing calculation on array
+Input - [10, 20, 30] ; multiply the input by 2
+Output - [20, 40, 60]
+*/
+//Method 1 - using
+let k = [];
+for (i = 0; i <= c.length - 1; i++) {
+  // i=0 ; 0 <= 3 - 1 = 2 ; h 10 ; i++
+  // i=1 ; 1 <= 3 - 1 = 2 ; 10 + 20 h = 30
+  //k = k.push(c[i] * 2);
+  console.log(k);
+}
+console.log(k);
